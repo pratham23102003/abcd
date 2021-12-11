693ed612bfa0f4568d0c5c2d7167796bef5c910a
pratham <vataliyapratham@gmail.com>
2021-12-11 14:34:57 +0530

    Commit message

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+ printf ("hello!");
+ printf(" my name is pratham");
+ printf(" i am from gujrat");
+ printf(" i completed my 12th from ss divine school");
+ printf (" i want to be softwere developer");
+return 0;
+}
